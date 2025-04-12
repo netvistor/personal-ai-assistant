3d24f67e7f7fee674de95c1f0ef6f91a23a4de70
netvistor <biuro@netvistor.pl>
2025-04-12 23:17:53 +0200

    task2.2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+// Aplikacja została podzielona na różne pliki, aby poprawić czytelność i zarządzanie kodem
+// klasa TelegramBotWrapper zajmuje się komunikacją z Telegramem
+// w klasie TelegramBotWrapper inicjowana jest klasa OpenAIService, która zajmuje się komunikacją z OpenAI API i obsługą odpowiedzi
+// W pliku index.js inicjalizowany jest bot Telegrama i uruchamiana jest aplikacja
 const TelegramBotWrapper = require('./services/telegram.bot');
 
 // Inicjalizacja bota
